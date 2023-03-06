ae59b182f419b437e610c23c233cb8dbb8ccae2c
Emilio Scodeler <emilio.scodeler@solides.com.br>
2023-03-06 12:23:24 -0300

    remove duplicate jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/**/*.test.ts'],
-    collectCoverageFrom: ['src/**']
-};
-exports["default"] = config;
